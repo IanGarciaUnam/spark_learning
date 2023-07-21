7cfc8028c4274f4662c877e5f5bf73486ffb4d8b
Ian Garcia <iangarcia@ciencias.unam.mx>
2023-07-20 19:37:56 -0600

    getNumPartitions and rdd

--- dataframes.py
+++ dataframes.py
@@ -11,8 +11,15 @@ df.columns
 #Schema
 
 from pyspark.sql.types import StructField, StructType, StringType, IntegerType
-schema =[StructField('edad', IntegerTyper(), True), StructField('nombre', StringType(),True)]
+schema =[StructField('edad', IntegerType(), True), StructField('nombre', StringType(),True)]
 schema_final = StructType(fields = schema)
-df = spark.read.json('dwsample1-json.json')
+df = spark.read.option("multiline","true").json('dwsample1-json.json')
 df.printSchema()
 df.show()
+
+print(df)
+
+rdd1=df.rdd
+
+print(rdd1)
+print(rdd1.getNumPartitions())
