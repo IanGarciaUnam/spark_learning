d152713d40647180dd5c98ca74bca0da4b80c74d
Ian Garcia <iangarcia@ciencias.unam.mx>
2023-07-26 17:48:04 -0600

    scala adding

---
+++ HelloYou.scala
@@ -0,0 +1,17 @@
+object HelloYou extends App{
+    if(args.size == 0)
+        println("Hello, you")
+    else
+        println("Hello," + args(0))
+    val name = "John Doe"
+    "hello".head
+    "hello".tail
+    "hello, world".take(5)
+    println("hi")
+    //val creates an immutable variable (like final in Java)
+    //var creates a mutable variable
+
+    val m  = ((1 + 2 )* 3)
+    val n  = (1 + 2) * 3
+    if (2 > 1) println("greater") else println("lesser")
+}
